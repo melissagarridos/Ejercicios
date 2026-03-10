6a7a499ea13a71c1e515682cb3cbda8a9b58ff84
Mel <melissagarridosierra@gmail.com>
2026-03-10 09:42:07 -0500

    Secuencia Fibonacci y Pascal

---
+++ ejercicio1.py
@@ -0,0 +1,24 @@
+# Ejercicio 1
+
+n = int(input("Ingresa numero de iteraciones: "))
+
+resultado = 2**n
+print("")
+print("El resultado de la piramide de Pascal es: ",resultado)
+
+
+
+# Ejercicio 2 
+print("")
+print("El resultado de la secuencia Fibonacci es: ")
+
+n0 = 0
+n1 = 1
+
+for i in range(0,n):
+
+    print((n1))  
+
+    n0, n1 = n1, n0 + n1
+
+    
